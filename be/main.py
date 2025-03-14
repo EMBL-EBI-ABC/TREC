91db34adbf192746214fa990da253fb974b6566e
Alexey Sokolov <alexey@ebi.ac.uk>
2025-03-14 14:41:03 +0000

    Initial BE

---
+++ be/main.py
@@ -0,0 +1,161 @@
+import os
+import urllib.parse
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI, HTTPException, Query, Path
+from elasticsearch import AsyncElasticsearch
+from fastapi.middleware.cors import CORSMiddleware
+from collections import defaultdict
+from typing import Annotated
+
+from models import (
+    get_list_of_aggregations,
+    ElasticResponse,
+    ElasticDetailsResponse,
+    TRECData,
+    TRECSearchParams,
+    TRECAggregationResponse,
+)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Initialize AsyncElasticsearch.
+    es_client = AsyncElasticsearch(
+        [os.getenv("ES_URL")],
+        http_auth=(os.getenv("ES_USERNAME"), os.getenv("ES_PASSWORD")),
+        verify_certs=True,
+    )
+    # Pass the client to the app's state so it's accessible in routes.
+    app.state.es_client = es_client
+    yield
+    # Clean up by closing the Elasticsearch client.
+    await es_client.close()
+
+
+# Initialize FastAPI with lifespan manager.
+app = FastAPI(
+    lifespan=lifespan,
+    title="TREC Data Portal API",
+    version="0.0.1",
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+)
+
+# Allow all origins.
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+
+# Generic search methods.
+
+
+async def elastic_search(index_name, params, data_class, aggregation_class):
+
+    # Build the query body based on whether there is full text search.
+    if params.q:
+        query_body = {"multi_match": {"query": params.q, "fields": ["*"]}}
+    else:
+        query_body = {"match_all": {}}
+
+    # Adding filters.
+    filters = []
+    aggregation_fields = get_list_of_aggregations(aggregation_class)
+    if aggregation_fields:
+        for aggregation_field in aggregation_fields:
+            filter_value = getattr(params, aggregation_field)
+            if filter_value:
+                filters.append({"terms": {aggregation_field: [filter_value]}})
+
+    # Combine query with filters.
+    search_body = {
+        "from": params.start,
+        "size": params.size,
+        "query": {
+            "bool": {
+                "must": query_body,
+                "filter": filters,
+            }
+        },
+        "aggs": defaultdict(dict),
+    }
+
+    # Adding aggregation fields.
+    if aggregation_fields:
+        for aggregation_field in aggregation_fields:
+            search_body["aggs"][aggregation_field] = {
+                "terms": {"field": aggregation_field}
+            }
+
+    # Adding sort field and sort order
+    search_body["sort"] = [{params.sort_field: {"order": params.sort_order}}]
+
+    # Performing the search.
+    try:
+        # Execute the async search request.
+        response = await app.state.es_client.search(index=index_name, body=search_body)
+        # Extract total count and hits.
+        total = response["hits"]["total"]["value"]
+        hits = [r["_source"] for r in response["hits"]["hits"]]
+        aggregations = response["aggregations"]
+        # Return the results.
+        return ElasticResponse[data_class, aggregation_class](
+            total=total,
+            start=params.start,
+            size=params.size,
+            results=hits,
+            aggregations=aggregations,
+        )
+
+    except Exception as e:
+        # Handle Elasticsearch errors.
+        raise HTTPException(status_code=500, detail=f"Search error: {str(e)}")
+
+
+async def elastic_details(index_name, record_id, data_class):
+    try:
+        # Quote the request, except for colons; they might appear in IDs.
+        quoted_id = urllib.parse.quote(record_id).replace("%3A", ":")
+        if ":" in quoted_id:
+            quoted_id = '"' + quoted_id + '"'
+        response = await app.state.es_client.search(
+            index=index_name, query={"term": {"_id": quoted_id}}
+        )
+        hits = [r["_source"] for r in response["hits"]["hits"]]
+        return ElasticDetailsResponse[data_class](results=hits)
+    except Exception as e:
+        # Handle Elasticsearch errors.
+        raise HTTPException(status_code=500, detail=f"Search error: {str(e)}")
+
+
+# MaveDB.
+
+
+@app.get("/data_portal/search")
+async def trec_search(
+    params: Annotated[TRECSearchParams, Query()],
+) -> ElasticResponse[TRECData, TRECAggregationResponse]:
+    return await elastic_search(
+        index_name="data_portal",
+        params=params,
+        data_class=TRECData,
+        aggregation_class=TRECAggregationResponse,
+    )
+
+
+@app.get("/data_portal_search/search/{record_id}")
+async def trec_details(
+    record_id: Annotated[str, Path(description="Record ID")],
+) -> ElasticDetailsResponse[TRECData]:
+    return await elastic_details(
+        index_name="data_portal",
+        record_id=record_id,
+        data_class=TRECData,
+    )
---
+++ be/models.py
@@ -0,0 +1,152 @@
+import datetime
+import types
+
+from pydantic import BaseModel, Field
+from typing import Generic, Literal, TypeVar
+
+T = TypeVar("T")  # Datasource data type
+A = TypeVar("A")  # Datasource aggregation type
+
+
+# Generic aggregation classes.
+
+
+class AggregationBucket(BaseModel):
+    key: int | str
+    doc_count: int
+
+
+class Aggregation(BaseModel):
+    doc_count_error_upper_bound: int
+    sum_other_doc_count: int
+    buckets: list[AggregationBucket]
+
+
+class CustomField(BaseModel):
+    name: str
+    value: str
+    unit: str
+
+
+class BioSamplesRelationships(BaseModel):
+    source: str
+    typ: str
+    target: str
+
+
+def get_list_of_aggregations(aggregation_class):
+    return sorted(aggregation_class.schema()["properties"].keys())
+
+
+# Generic Elastic response classes.
+
+
+class ElasticResponse(BaseModel, Generic[T, A]):
+    total: int
+    start: int
+    size: int
+    results: list[T]
+    aggregations: A
+
+
+class ElasticDetailsResponse(BaseModel, Generic[T]):
+    results: list[T]
+
+
+# Base Elastic query class.
+
+
+class SearchParams(BaseModel):
+    model_config = {
+        "populate_by_name": True,
+        "extra": "forbid",
+    }
+    # Basic query parameters.
+    q: str | None = Field(None, description="Search query string")
+    start: int = Field(0, description="Starting point of the results")
+    size: int = Field(10, gt=0, description="Number of results per page")
+    # No sorting by default, child classes can override this.
+    sort_field: str | None = None
+    sort_order: Literal["desc", "asc"] = "asc"
+
+
+# Datasource definition.
+
+
+class FieldDefinition:
+    def __init__(self, name: str, type: type|types.UnionType, filterable: bool = False):
+        self.name = name
+        self.type = type
+        self.filterable = filterable
+
+
+class DataSource:
+    def __init__(
+            self,
+            name: str,
+            fields: list[FieldDefinition],
+            default_sort_field: str,
+            default_sort_order: Literal["desc", "asc"],
+    ):
+        self.name = name
+        self.fields = fields
+        self.default_sort_field = default_sort_field
+        self.default_sort_order = default_sort_order
+
+    def generate_classes(self):
+        fields = {field.name: (field.type, field.filterable) for field in self.fields}
+
+        class Data(BaseModel):
+            __annotations__ = {name: type for name, (type, _) in fields.items()}
+
+        class AggregationResponse(BaseModel):
+            __annotations__ = {
+                name: Aggregation
+                for name, (_, filterable) in fields.items()
+                if filterable
+            }
+
+        class SearchParamsExtended(SearchParams):
+            # Define filterable fields with default values
+            locals().update(
+                {
+                    name: Field(None, description=f"{name} query", alias=name)
+                    for name, (type_, filterable) in fields.items()
+                    if filterable
+                }
+            )
+            __annotations__ = {
+                name: type_ | None
+                for name, (type_, filterable) in fields.items()
+                if filterable
+            }
+            # Define default sort field and order.
+            sort_field: str | None = Field(
+                self.default_sort_field, description="Sort field"
+            )
+            sort_order: Literal["desc", "asc"] = Field(
+                self.default_sort_order, description="Sort order"
+            )
+
+        return Data, AggregationResponse, SearchParamsExtended
+
+
+# MaveDB.
+trec = DataSource(
+    name="TREC",
+    fields=[
+        FieldDefinition(name="altitude", type=str, filterable=True),
+        FieldDefinition(name="collection date", type=datetime.datetime),
+        FieldDefinition(name="depth", type=str, filterable=True),
+        FieldDefinition(name="geographic location (country and/or sea)", type=str,
+                        filterable=True),
+        FieldDefinition(name="geographic location (latitude)", type=float),
+        FieldDefinition(name="geographic location (longitude)", type=float),
+        FieldDefinition(name="organism", type=str, filterable=True),
+        FieldDefinition(name="customFields", type=list[CustomField]|None),
+        FieldDefinition(name="relationships", type=list[BioSamplesRelationships]|None),
+    ],
+    default_sort_field="collection date",
+    default_sort_order="desc",
+)
+TRECData, TRECAggregationResponse, TRECSearchParams = trec.generate_classes()
