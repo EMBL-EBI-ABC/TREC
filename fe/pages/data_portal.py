d931849ee89cb76f95fc5be5cda0cb549090aed1
Alexey Sokolov <alexey@ebi.ac.uk>
2025-03-17 20:38:25 +0000

    Removing spaces aroun -

--- fe/pages/data_portal.py
+++ fe/pages/data_portal.py
@@ -153,7 +153,7 @@ def create_update_data_table(organism_filter, depth_filter, altitude_filter,
     if pagination is None or pagination == 1:
         params = {"size": 30, "start": 0}
     else:
-        params = {"size": 30, "start": (pagination - 1) * 30}
+        params = {"size": 30, "start": (pagination-1) * 30}
     for field_name, values in {"organism": organism_filter, "depth": depth_filter,
                                "altitude": altitude_filter,
                                "location": location_filter}.items():
